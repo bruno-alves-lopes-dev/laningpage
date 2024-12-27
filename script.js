3bf296c96d1b8f9beec1f4685d9847ef71fc32a6
brno lopes <bruno.alves.lopes.dev@gmail.com>
2024-12-27 14:50:14 -0300

    cores

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-function toggleMode() {
-  const html = document.documentElement
+const switchButton = document.getElementById("switch")
+const body = document.body
 
-  html.classList.toggle('light')
+switchButton.addEventListener("click", () => {
+  body.classList.toggle("light")
+})
+
+function createSnowflake() {
+  const snowflake = document.createElement("div")
+  snowflake.classList.add("snowflake")
+
+  snowflake.style.left = `${Math.random() * 100}vw`
+
+  snowflake.style.animationDuration = `${Math.random() * 4 + 4}s`
+
+  snowflake.style.animationDelay = `${Math.random() * 5}s`
+
+  snowflake.textContent = "*" //
+
+  snowflake.style.fontSize = `${Math.random() * 5 + 10}px`
+
+  document.body.appendChild(snowflake)
+
+  setTimeout(() => {
+    snowflake.remove()
+  }, 10000)
 }
+
+setInterval(createSnowflake, 50)
